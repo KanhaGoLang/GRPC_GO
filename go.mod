db8c60a707960e433bf01667e42c3beefe5a96e9
Sanjeev Kumar <sanjeevscet@gmail.com>
2024-03-26 16:45:15 +0530

    Saving user in user table

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/KanhaGoLang/grpc_go
 go 1.20
 
 require (
+	github.com/go-sql-driver/mysql v1.8.0
 	google.golang.org/grpc v1.62.1
 	google.golang.org/protobuf v1.33.0
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
--- server/server.go
+++ server/server.go
@@ -2,35 +2,55 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"net"
 
-	user "github.com/KanhaGoLang/grpc_go/proto"
 	"google.golang.org/grpc"
+
+	user "github.com/KanhaGoLang/grpc_go/proto"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+type UserService struct {
+	db *sql.DB
+}
+
+func NewUserService(db *sql.DB) *UserService {
+	return &UserService{db}
+}
+
 type userServer struct {
+	userService *UserService
 	user.UnimplementedUserServiceServer
 }
 
 func main() {
+
+	// Initialize database connection
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/sanjeev")
+	if err != nil {
+		fmt.Println("Error connecting to the database:", err)
+		return
+	}
+	defer db.Close()
+
+	// Initialize UserService
+	userService := NewUserService(db)
+
 	listener, tcpErr := net.Listen("tcp", "localhost:50052")
 	if tcpErr != nil {
 		panic(tcpErr)
 	}
 
 	grpcServer := grpc.NewServer()
-	user.RegisterUserServiceServer(grpcServer, &userServer{})
+	user.RegisterUserServiceServer(grpcServer, &userServer{userService: userService})
 
 	fmt.Println("Server started")
 
 	if e := grpcServer.Serve(listener); e != nil {
-		fmt.Println("panic started")
-
 		panic(e)
 	}
-
-	fmt.Println("Server started")
 
 }
 
@@ -45,4 +65,39 @@ func (u *userServer) ReadUserById(ctx context.Context, req *user.UserId) (*user.
 		Role:     "admin",
 		IsActive: true,
 	}, nil
+}
+
+func (u *userServer) CreateUser(ctx context.Context, req *user.User) (*user.User, error) {
+	fmt.Println("Server create user")
+
+	createdUser, err := u.userService.CreateUser(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(createdUser)
+
+	return createdUser, nil
+}
+
+func (s *UserService) CreateUser(ctx context.Context, user *user.User) (*user.User, error) {
+	fmt.Println("Service create user")
+
+	query := "INSERT INTO users (name, email, password, role, is_active, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)"
+
+	result, err := s.db.ExecContext(ctx, query, user.Name, user.Email, user.Password, user.Role, user.IsActive, user.CreatedAt, user.UpdatedAt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	userId, err := result.LastInsertId()
+
+	if err != nil {
+		return nil, err
+	}
+	user.Id = int32(userId)
+
+	return user, nil
+
 }
